9205c192e373845d4e7eabb1f1e3b2d0479c8010
Mariana7IT <153327568+Mariana7IT@users.noreply.github.com>
2024-07-17 22:30:02 +0300

    loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,8 +1,6 @@
 import React from "react";
-import Loader from "react-loader-spinner";
+import { ThreeDots } from "react-loader-spinner";
 
-const Loading = () => (
-  <Loader type="ThreeDots" color="#00BFFF" height={80} width={80} />
-);
+const Loader = () => <ThreeDots color="#00BFFF" height={80} width={80} />;
 
-export default Loading;
+export default Loader;
