8685d8ade53ab85243bfba593a6b7e7c22e30575
Mariana7IT <153327568+Mariana7IT@users.noreply.github.com>
2024-07-18 23:20:19 +0300

    loder_btn

--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -2,9 +2,11 @@ import s from "./LoadMoreBtn.module.css";
 
 const LoadMoreBtn = ({ onClick }) => {
   return (
-    <button className={s.button} onClick={onClick}>
-      Load more
-    </button>
+    <div className={s.buttonWrapper}>
+      <button className={s.button} onClick={onClick}>
+        Load more
+      </button>
+    </div>
   );
 };
 
