3aac76e6d5373a99f99d81d019d562d816bee74f
Mariana7IT <153327568+Mariana7IT@users.noreply.github.com>
2024-07-19 20:55:05 +0300

    image card

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -2,15 +2,11 @@ import React from "react";
 import s from "./ImageCard.module.css";
 
 const ImageCard = ({ image, onClick }) => {
-    return (
-    <div className={s.card} onClick={onClick}>
-        <img
-        src={image.urls.small}
-        alt={image.alt_description}
-        className={s.image}
-        />
-    </div>
-);
+  return (
+    <li className={s.card} onClick={() => onClick(image)}>
+      <img src={image.urls.small} alt={image.alt_description} className={s.image} />
+    </li>
+  );
 };
 
 export default ImageCard;
