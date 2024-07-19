aeb178fec84c310eeafbb2248e154d76989f45c8
Mariana7IT <153327568+Mariana7IT@users.noreply.github.com>
2024-07-19 16:18:54 +0300

    onImageClick

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import ImageCard from "../ImageCard/ImageCard";
 import s from "./ImageGallery.module.css";
 
+
 const ImageGallery = ({ images, onImageClick }) => {
   return (
     <div className={s.imageGallery}>
       {images.map((image) => (
-        <div key={image.id} className={s.imageGalleryItem}>
+        <div
+          key={image.id}
+          className={s.imageGalleryItem}
+          onClick={() => onImageClick(image)}
+        >
           <img src={image.urls.small} alt={image.alt_description} />
         </div>
       ))}
