e2a4268df90e245eb56218c5531297bb75c3b22c
Mariana7IT <153327568+Mariana7IT@users.noreply.github.com>
2024-07-22 22:38:17 +0300

    ImageCard

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -3,11 +3,12 @@ import s from "./ImageCard.module.css";
 
 const ImageCard = ({ image, onImageClick }) => {
   return (
-    <div className={s.imageCard} onClick={() => onImageClick(image)}>
+    <div className={s.imageCard}>
       <img
         src={image.urls.small}
         alt={image.alt_description}
         className={s.image}
+        onClick={() => onImageClick(image)}
       />
     </div>
   );
